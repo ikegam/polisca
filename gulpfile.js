2f1dcd742f961071e1d31440c43784658dcceb46
Hiroyuki Ikegami <ikegam@mixallow.net>
2016-06-21 12:21:59 +0900

    update gulpfile and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+
 
 gulp.task('webserver', function() {
   gulp.src('./')
@@ -10,4 +15,17 @@ gulp.task('webserver', function() {
    );
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('browserify', function() {
+  browserify('./index.js', { debug: true })
+    .transform(babelify, {presets: ["react"]})
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./index.js', ['browserify'])
+});
+
+gulp.task('default', ['webserver', 'browserify', 'watch']);
